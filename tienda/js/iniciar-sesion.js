70be1d9e4fefe7f57d2ebc96bba85621b7887eff
btorresd <b.torresd@duocuc.cl>
2025-09-12 14:47:24 -0300

    Se agrega el html de inicio-sesion y tambien el js que incluye el formulario y sus validaciones

---
+++ tienda/js/iniciar-sesion.js
@@ -0,0 +1,39 @@
+function validarFormularioInicio(){
+ let errores = [];
+
+ let email = document.getElementById("email").value.trim()
+ let clave = document.getElementById("clave").value;
+
+ // Validaciones
+
+ if (email === "") {
+    errores.push("Porfavor ingrese un email valido")
+ }
+ if (clave === "") {
+    errores.push("Porfavor ingrese una clave valida")
+ }
+  // Usuarios definidos en el código
+  const usuarios = [
+    { email: "admin@duoc.cl", clave: "admin123", rol: "admin" },
+    { email: "cliente@gmail.com", clave: "cliente456", rol: "cliente" }
+  ];
+
+  // Buscar coincidencia
+  const usuarioValido = usuarios.find(
+    (usuario) => usuario.email === email && usuario.clave === clave
+  );
+
+  if (usuarioValido) {
+    mensajesDiv.innerHTML = `<div class="alert alert-success">✅ Inicio de sesión exitoso</div>`;
+
+    // Redirigir según el rol
+    if (usuarioValido.rol === "admin") {
+      window.location.href = "../../administrador/paginas/home.html";
+
+    } else {
+      window.location.href = "../../index.html";
+    }
+  } else {
+    mensajesDiv.innerHTML = `<div class="alert alert-danger">❌ Correo o clave incorrectos</div>`;
+  }
+}
